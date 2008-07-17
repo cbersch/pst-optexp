b41a42ba2db9b45ed3c4dd4ac96dbbe2c82a9a32
Christoph Bersch <christoph.bersch@mpl.mpg.de>
2008-07-17 14:55:04 +0000

    * connect two named components after definition time * added new components    \optprism    \fibercollimator

---
+++ extractoptions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import re
+def compare(x, y):
+    a=x[0].lower()
+    b=y[0].lower()
+    if (a < b):
+        return -1
+    elif (a > b):
+        return 1
+    else:
+        return 0
+
+doc=file('pst-optexp-doc.tex').read()
+pat=re.compile(r"\\item\[\\param\{(.*?)\}:\]\s*\\paramvalue\{(.*?)\}\s*\(\\defaultparam\{(.*?)\}\)")
+
+match=pat.findall(doc)
+match.sort(cmp=lambda x,y: cmp(x[0].lower(), y[0].lower()))
+
+for o in match:
+    print o[0] + " & " + o[1] + " & " + o[2] + r"\\"
+
