edc7197bc4744e31ffcbc050783efe7a1cca95c6
Christoph Bersch <usenet@bersch.net>
2008-07-25 14:02:34 +0000

    * completed the documentation * fixed some small typos and bugs * should be ready for release now :-)

--- extractoptions.py
+++ extractoptions.py
@@ -12,11 +12,19 @@ def compare(x, y):
         return 0
 
 doc=file('pst-optexp-doc.tex').read()
-pat=re.compile(r"\\paramitem\{(.*?)\}\{(.*?)\}\{(.*?)\}\n")
-
+pat=re.compile(r"\\paramitem\{([a-z].*?)\}\{(.*?)\}\{(.*?)\}\n")
+pat2=re.compile(r"^\\item\[\\param\{(.*?)\}:\]\s*(.*?)\n", re.M)
 match=pat.findall(doc)
+match2=pat2.findall(doc)
+for p in match2:
+    match.append(p)
+
 match.sort(cmp=lambda x,y: cmp(x[0].lower(), y[0].lower()))
 
 for o in match:
-    print o[0] + " & " + o[1] + " & " + o[2] + r"\\"
+    if len(o) == 3:
+        print o[0] + " & " + o[1] + " & " + o[2] + r"\\"
+    else:
+        print o[0] + " & \multicolumn{2}{l}{" + o[1] + r"\\"
+
 
