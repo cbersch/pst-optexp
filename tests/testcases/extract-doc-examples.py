b28363042457e2d598e29fb0effb039d7f680d25
Christoph Bersch <usenet@bersch.net>
2012-03-02 13:33:00 +0000

    Renamed option 'beamangleabs' to 'beamref' Renamed option 'fiberangle' to 'fiberref' Added option 'fiberstyle' to choose between all \nc* connection types. Worked on documentation. Adapted tests to new options.

--- tests/testcases/extract-doc-examples.py
+++ tests/testcases/extract-doc-examples.py
@@ -22,6 +22,7 @@ f.write(r"""\documentclass[a4paper]{scrreprt}
 \newcommand{\poeTR}[1]{\TR{\ttfamily#1}}
 \newcommand{\opt}[1]{\texttt{#1}}
 \definecolor{spot}{rgb}{1,.4,.2}%
+\colorlet{DOrange}{spot}
 \newpsstyle{Refline}{linecolor=gray!60}
 \colorlet{Refline}{gray!60}
 \newif\ifGERMAN\GERMANfalse
